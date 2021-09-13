3df6457105d65a5c9974b2dda93203341ed0acdb
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-09-13 11:58:12 +0200

    debut react job 01

---
+++ calculator9000/src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+const title = 'Calculator 9000'; 
+
+function TheTitle() {
+  return (
+    <h1> {title}</h1>
+  );
+}
+
+export default TheTitle;
