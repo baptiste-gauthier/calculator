c900da95ff3ef39c66733125087f871a8918815c
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-09-14 16:59:28 +0200

    calculator

--- calculator9000/src/Component/BeautifullScreen.js
+++ calculator9000/src/Component/BeautifullScreen.js
@@ -4,7 +4,7 @@ import AmazingNumberButton from './AmazingNumberButton';
 
 function BeautifullScreen() {
 
-    const [string, setString] = useState("") ;
+    const [string, setString] = useState("0") ;
     return( 
     <div>
         <div className="calculator_screen">
